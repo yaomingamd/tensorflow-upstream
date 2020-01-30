c97c9b04fb11ed62d888e6daaaebb7765d10ef18
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2020-01-30 03:40:38 -0800

    Bugfixes

--- tensorflow/core/kernels/cwise_op_fma.h
+++ tensorflow/core/kernels/cwise_op_fma.h
@@ -16,91 +16,140 @@ limitations under the License.
 #ifndef TENSORFLOW_CORE_KERNELS_CWISE_OP_FMA_H_
 #define TENSORFLOW_CORE_KERNELS_CWISE_OP_FMA_H_
 
+
 #define EIGEN_USE_THREADS
 
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/platform/types.h"
 
+
 namespace tensorflow {
 
-template <typename Device, typename T, int SGN>
-class LaunchFusedMulAddOp {
- public:
-  void operator()(const Device& device, T* out, const T* x1, const T* y1,
-                  const T* x2, uint64 elements, bool broadcast_x1,
-                  bool broadcast_y1, bool broadcast_x2);
-};
+enum FMAType {
+	FMAType_Add,
+	FMAType_Sub,
+	FMAType_SubRev
+};	
 
-template <typename Device, typename T, int SGN>
-class LaunchFusedMulAdd2Op {
- public:
-  void operator()(const Device& device, T* out, const T* x1, const T* y1,
-                  const T* x2, const T* y2, uint64 elements, bool broadcast_x1,
-                  bool broadcast_y1, bool broadcast_x2, bool broadcast_y2);
-};
+template <typename Device, typename T, FMAType Type>
+class LaunchFusedMulAddOp
+{
+public:
+	void operator()(const Device& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2,
+		uint64 elements, 
+		bool broadcast_x1, bool broadcast_y1, 
+		bool broadcast_x2);
+}; 
 
-template <typename Device, typename T, int SGN>
-class FallbackLaunchFusedMulAddOp {
- public:
-  void operator()(const Device& device, T* out, const T* x1, const T* y1,
-                  const T* x2, int64 dims[5], uint8 broadcast_masks[5]);
-};
+template <typename Device, typename T, FMAType Type>
+class LaunchFusedMulAdd2Op
+{
+public:
+	void operator()(const Device& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2, const T* y2,
+		uint64 elements,
+		bool broadcast_x1, bool broadcast_y1, 
+		bool broadcast_x2, bool broadcast_y2);
+}; 
 
-template <typename Device, typename T, int SGN>
-class FallbackLaunchFusedMulAdd2Op {
- public:
-  void operator()(const Device& device, T* out, const T* x1, const T* y1,
-                  const T* x2, const T* y2, int64 dims[5],
-                  uint8 broadcast_masks[5]);
-};
+
+template <typename Device, typename T, FMAType Type>
+class FallbackLaunchFusedMulAddOp
+{
+public:
+	void operator()(const Device& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2,
+		int64 dims[5],
+		uint8 broadcast_masks[5]);
+}; 
+
+template <typename Device, typename T, FMAType Type>
+class FallbackLaunchFusedMulAdd2Op
+{
+public:
+	void operator()(const Device& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2, const T* y2,
+		int64 dims[5],
+		uint8 broadcast_masks[5]);
+}; 
+
 
 #if GOOGLE_CUDA || TENSORFLOW_USE_ROCM
 typedef Eigen::GpuDevice GPUDevice;
 
-template <typename T, int SGN>
-class LaunchFusedMulAddOp<GPUDevice, T, SGN> {
- public:
-  typedef void exec_fun(const GPUDevice& device, T* out, const T* x1,
-                        const T* y1, const T* x2, uint64 elements);
+template <typename T, FMAType Type>
+class LaunchFusedMulAddOp<GPUDevice, T, Type>
+{
+public:
+	typedef void exec_fun(const GPUDevice& device, T* out,
+		const T* x1, const T* y1, const T* x2,
+		uint64 elements);
 
-  template <int N>
-  static void execute(const GPUDevice& device, T* out, const T* x1, const T* y1,
-                      const T* x2, uint64 elements);
+	template <int N>
+	static void execute(const GPUDevice& device, T* out,
+		const T* x1, const T* y1, const T* x2,
+		uint64 elements);
 
-  void operator()(const GPUDevice& device, T* out, const T* x1, const T* y1,
-                  const T* x2, uint64 elements, bool broadcast_x1,
-                  bool broadcast_y1, bool broadcast_x2);
+	void operator()(const GPUDevice& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2,
+		uint64 elements,
+		bool broadcast_x1, bool broadcast_y1, 
+		bool broadcast_x2);
+}; 
+
+template <typename T, FMAType Type>
+class LaunchFusedMulAdd2Op<GPUDevice, T, Type>
+{
+public:
+	typedef void exec_fun(const GPUDevice& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2, const T* y2,
+		uint64 elements);
+	template <int N>
+	static void execute(const GPUDevice& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2, const T* y2,
+		uint64 elements);
+	void operator()(const GPUDevice& device,
+		T* out,
+		const T* x1, const T* y1, const T* x2, const T* y2,
+		uint64 elements,
+		bool broadcast_x1, bool broadcast_y1, 
+		bool broadcast_x2, bool broadcast_y2);
 };
 
-template <typename T, int SGN>
-class LaunchFusedMulAdd2Op<GPUDevice, T, SGN> {
- public:
-  typedef void exec_fun(const GPUDevice& device, T* out, const T* x1,
-                        const T* y1, const T* x2, const T* y2, uint64 elements);
-  template <int N>
-  static void execute(const GPUDevice& device, T* out, const T* x1, const T* y1,
-                      const T* x2, const T* y2, uint64 elements);
-  void operator()(const GPUDevice& device, T* out, const T* x1, const T* y1,
-                  const T* x2, const T* y2, uint64 elements, bool broadcast_x1,
-                  bool broadcast_y1, bool broadcast_x2, bool broadcast_y2);
-};
+template <typename T, FMAType Type>
+class FallbackLaunchFusedMulAddOp<GPUDevice, T, Type>
+{
+public:
+  void operator()(const GPUDevice& device,
+    T* out,
+    const T* x1, const T* y1, const T* x2,
+    int64 dims[5],
+    uint8 broadcast_masks[5]);
+}; 
 
-template <typename T, int SGN>
-class FallbackLaunchFusedMulAddOp<GPUDevice, T, SGN> {
- public:
-  void operator()(const GPUDevice& device, T* out, const T* x1, const T* y1,
-                  const T* x2, int64 dims[5], uint8 broadcast_masks[5]);
-};
-
-template <typename T, int SGN>
-class FallbackLaunchFusedMulAdd2Op<GPUDevice, T, SGN> {
- public:
-  void operator()(const GPUDevice& device, T* out, const T* x1, const T* y1,
-                  const T* x2, const T* y2, int64 dims[5],
-                  uint8 broadcast_masks[5]);
-};
+template <typename T, FMAType Type>
+class FallbackLaunchFusedMulAdd2Op<GPUDevice, T, Type>
+{
+public:
+  void operator()(const GPUDevice& device,
+    T* out,
+    const T* x1, const T* y1, const T* x2, const T* y2,
+    int64 dims[5],
+    uint8 broadcast_masks[5]);
+}; 
 #endif
 
-};  // namespace tensorflow
+
+
+};
 
 #endif
+
--- tensorflow/core/kernels/cwise_op_gpu_fma.cu.cc
+++ tensorflow/core/kernels/cwise_op_gpu_fma.cu.cc
@@ -23,25 +23,29 @@ namespace tensorflow {
 
 typedef Eigen::GpuDevice GPUDevice;
 
-template <typename T, int N, int SGN>
+template <FMAType Type, typename T> __device__ T fma_op(T m1, T m2)
+{
+    if (Type==FMAType_Add)
+      return m1 + m2;
+    else if (Type==FMAType_Sub)
+      return m1 - m2;
+    else
+      return m2 - m1;
+}
+
+template <typename T, int N, FMAType Type>
 __global__ void CwiseFusedMulAddKernel(GpuLaunchConfig cfg, T* out, const T* x1,
                                        const T* y1, const T* x2) {
   constexpr bool broadcast_x1 = (N & 1);
   constexpr bool broadcast_y1 = (N & 2);
   constexpr bool broadcast_x2 = (N & 4);
   GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
-    T m1 = x1[broadcast_x1 ? 0 : i] * y1[broadcast_y1 ? 0 : i];
-    T m2 = x2[broadcast_x2 ? 0 : i];
-    if (SGN == 1)
-      out[i] = m1 + m2;
-    else if (SGN == -1)
-      out[i] = m1 - m2;
-    else
-      out[i] = m2 - m1;
+    out[i]=fma_op<Type>(x1[broadcast_x1 ? 0 : i] * y1[broadcast_y1 ? 0 : i],
+             x2[broadcast_x2 ? 0 : i]);
   }
 }
 
-template <typename T, int N, int SGN>
+template <typename T, int N, FMAType Type>
 __global__ void CwiseFusedMulAdd2Kernel(GpuLaunchConfig cfg, T* out,
                                         const T* x1, const T* y1, const T* x2,
                                         const T* y2) {
@@ -50,27 +54,27 @@ __global__ void CwiseFusedMulAdd2Kernel(GpuLaunchConfig cfg, T* out,
   constexpr bool broadcast_x2 = (N & 4);
   constexpr bool broadcast_y2 = (N & 8);
   GPU_1D_KERNEL_LOOP(i, cfg.virtual_thread_count) {
-    T m1 = x1[broadcast_x1 ? 0 : i] * y1[broadcast_y1 ? 0 : i];
-    T m2 = x2[broadcast_x2 ? 0 : i] * y2[broadcast_y2 ? 0 : i];
-    out[i] = (SGN > 0) ? m1 + m2 : m1 - m2;
+      out[i]=fma_op<Type>(
+        x1[broadcast_x1 ? 0 : i] * y1[broadcast_y1 ? 0 : i],
+        x2[broadcast_x2 ? 0 : i] * y2[broadcast_y2 ? 0 : i]);
   }
 }
 
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 template <int N>
-void LaunchFusedMulAddOp<GPUDevice, T, SGN>::execute(const GPUDevice& device,
+void LaunchFusedMulAddOp<GPUDevice, T, Type>::execute(const GPUDevice& device,
                                                      T* out, const T* x1,
                                                      const T* y1, const T* x2,
                                                      uint64 elements) {
   // printf("LaunchFusedMulAddOp<GPU,%d>(%ld)\n", N, elements);
   auto config = GetGpuLaunchConfig(elements, device);
-  TF_CHECK_OK(GpuLaunchKernel(CwiseFusedMulAddKernel<T, N, SGN>,
+  TF_CHECK_OK(GpuLaunchKernel(CwiseFusedMulAddKernel<T, N, Type>,
                               config.block_count, config.thread_per_block, 0,
                               device.stream(), config, out, x1, y1, x2));
 }
 
-template <typename T, int SGN>
-void LaunchFusedMulAddOp<GPUDevice, T, SGN>::operator()(
+template <typename T, FMAType Type>
+void LaunchFusedMulAddOp<GPUDevice, T, Type>::operator()(
     const GPUDevice& device, T* out, const T* x1, const T* y1, const T* x2,
     uint64 elements, bool broadcast_x1, bool broadcast_y1, bool broadcast_x2) {
   int index =
@@ -80,22 +84,22 @@ void LaunchFusedMulAddOp<GPUDevice, T, SGN>::operator()(
   execs[index](device, out, x1, y1, x2, elements);
 }
 
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 template <int N>
-void LaunchFusedMulAdd2Op<GPUDevice, T, SGN>::execute(const GPUDevice& device,
+void LaunchFusedMulAdd2Op<GPUDevice, T, Type>::execute(const GPUDevice& device,
                                                       T* out, const T* x1,
                                                       const T* y1, const T* x2,
                                                       const T* y2,
                                                       uint64 elements) {
   // printf("LaunchFusedMulAdd2Op<GPU,%d>(%ld)\n", N, elements);
   auto config = GetGpuLaunchConfig(elements, device);
-  TF_CHECK_OK(GpuLaunchKernel(CwiseFusedMulAdd2Kernel<T, N, SGN>,
+  TF_CHECK_OK(GpuLaunchKernel(CwiseFusedMulAdd2Kernel<T, N, Type>,
                               config.block_count, config.thread_per_block, 0,
                               device.stream(), config, out, x1, y1, x2, y2));
 }
 
-template <typename T, int SGN>
-void LaunchFusedMulAdd2Op<GPUDevice, T, SGN>::operator()(
+template <typename T, FMAType Type>
+void LaunchFusedMulAdd2Op<GPUDevice, T, Type>::operator()(
     const GPUDevice& device, T* out, const T* x1, const T* y1, const T* x2,
     const T* y2, uint64 elements, bool broadcast_x1, bool broadcast_y1,
     bool broadcast_x2, bool broadcast_y2) {
@@ -117,8 +121,7 @@ struct Fallback_FMA_Arg {
   uint32 broadcast_mask;
 };
 
-
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 __global__ void FallbackLaunchFusedMulAddKernel(T* out, const T* x1,
                                                 const T* y1, const T* x2,
                                                 Fallback_FMA_Arg<4> arg) {
@@ -139,15 +142,9 @@ __global__ void FallbackLaunchFusedMulAddKernel(T* out, const T* x1,
 
   for (int32 y = threadIdx.y; y < arg.dims[1]; y += blockDim.y) {
     for (int32 x = threadIdx.x; x < arg.dims[0]; x += blockDim.x) {
-      T m1 = x1[(arg.broadcast_mask & 1) ? x : 0] *
-             y1[(arg.broadcast_mask & 2) ? x : 0];
-      T m2 = x2[(arg.broadcast_mask & 4) ? x : 0];
-      if (SGN == 1)
-        out[x] = m1 + m2;
-      else if (SGN == -1)
-        out[x] = m1 - m2;
-      else
-        out[x] = m2 - m1;
+      out[x]=fma_op<Type>(x1[(arg.broadcast_mask & 1) ? x : 0] *
+             y1[(arg.broadcast_mask & 2) ? x : 0],
+            x2[(arg.broadcast_mask & 4) ? x : 0]);
     }
     out += blockDim.y * arg.shifts[0][0];
     x1 += blockDim.y * arg.shifts[1][0];
@@ -156,18 +153,18 @@ __global__ void FallbackLaunchFusedMulAddKernel(T* out, const T* x1,
   }
 }
 
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 __global__ void FallbackLaunchFusedMulAddKernel4D(T* out, const T* x1,
-                                                const T* y1, const T* x2,
-                                                Fallback_FMA_Arg<4> arg) {
+                                                  const T* y1, const T* x2,
+                                                  Fallback_FMA_Arg<4> arg) {
   int32 v = blockIdx.y;
   int32 z = threadIdx.z + blockIdx.x * blockDim.z;
   if (z >= arg.dims[2]) return;
 
   out += arg.shifts[0][1] * z + arg.shifts[0][2] * v;
-  x1 += arg.shifts[1][1] * z + arg.shifts[1][2] * v ;
-  y1 += arg.shifts[2][1] * z + arg.shifts[2][2] * v ;
-  x2 += arg.shifts[3][1] * z + arg.shifts[3][2] * v ;
+  x1 += arg.shifts[1][1] * z + arg.shifts[1][2] * v;
+  y1 += arg.shifts[2][1] * z + arg.shifts[2][2] * v;
+  x2 += arg.shifts[3][1] * z + arg.shifts[3][2] * v;
 
   out += threadIdx.y * arg.shifts[0][0];
   x1 += threadIdx.y * arg.shifts[1][0];
@@ -175,16 +172,11 @@ __global__ void FallbackLaunchFusedMulAddKernel4D(T* out, const T* x1,
   x2 += threadIdx.y * arg.shifts[3][0];
 
   for (int32 y = threadIdx.y; y < arg.dims[1]; y += blockDim.y) {
-    for (int32 x = threadIdx.x + blockDim.x*blockIdx.z; x < arg.dims[0]; x += blockDim.x*gridDim.z) {
-      T m1 = x1[(arg.broadcast_mask & 1) ? x : 0] *
-             y1[(arg.broadcast_mask & 2) ? x : 0];
-      T m2 = x2[(arg.broadcast_mask & 4) ? x : 0];
-      if (SGN == 1)
-        out[x] = m1 + m2;
-      else if (SGN == -1)
-        out[x] = m1 - m2;
-      else
-        out[x] = m2 - m1;
+    for (int32 x = threadIdx.x + blockDim.x * blockIdx.z; x < arg.dims[0];
+         x += blockDim.x * gridDim.z) {
+      out[x]=fma_op<Type>(x1[(arg.broadcast_mask & 1) ? x : 0] *
+              y1[(arg.broadcast_mask & 2) ? x : 0],
+              x2[(arg.broadcast_mask & 4) ? x : 0]);
     }
     out += blockDim.y * arg.shifts[0][0];
     x1 += blockDim.y * arg.shifts[1][0];
@@ -193,7 +185,7 @@ __global__ void FallbackLaunchFusedMulAddKernel4D(T* out, const T* x1,
   }
 }
 
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 __global__ void FallbackLaunchFusedMulAddKernel2D(T* out, const T* x1,
                                                   const T* y1, const T* x2,
                                                   Fallback_FMA_Arg<4> arg) {
@@ -203,19 +195,14 @@ __global__ void FallbackLaunchFusedMulAddKernel2D(T* out, const T* x1,
   x2 += blockIdx.x * arg.shifts[3][0];
 
   for (int32 x = threadIdx.x; x < arg.dims[0]; x += blockDim.x) {
-    T m1 = x1[(arg.broadcast_mask & 1) ? x : 0] *
-           y1[(arg.broadcast_mask & 2) ? x : 0];
-    T m2 = x2[(arg.broadcast_mask & 4) ? x : 0];
-    if (SGN == 1)
-      out[x] = m1 + m2;
-    else if (SGN == -1)
-      out[x] = m1 - m2;
-    else
-      out[x] = m2 - m1;
+    out[x]=fma_op<Type>(x1[(arg.broadcast_mask & 1) ? x : 0] *
+             y1[(arg.broadcast_mask & 2) ? x : 0],
+             x2[(arg.broadcast_mask & 4) ? x : 0]);
+    
   }
 }
 
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 __global__ void FallbackLaunchFusedMulAdd2Kernel(T* out, const T* x1,
                                                  const T* y1, const T* x2,
                                                  const T* y2,
@@ -239,11 +226,11 @@ __global__ void FallbackLaunchFusedMulAdd2Kernel(T* out, const T* x1,
 
   for (int32 y = threadIdx.y; y < arg.dims[1]; y += blockDim.y) {
     for (int32 x = threadIdx.x; x < arg.dims[0]; x += blockDim.x) {
-      T m1 = x1[(arg.broadcast_mask & 1) ? x : 0] *
-             y1[(arg.broadcast_mask & 2) ? x : 0];
-      T m2 = x2[(arg.broadcast_mask & 4) ? x : 0] *
-             y2[(arg.broadcast_mask & 8) ? x : 0];
-      out[x] = (SGN > 0) ? m1 + m2 : m1 - m2;
+      out[x]=fma_op<Type>(x1[(arg.broadcast_mask & 1) ? x : 0] *
+             y1[(arg.broadcast_mask & 2) ? x : 0],
+             x2[(arg.broadcast_mask & 4) ? x : 0] *
+             y2[(arg.broadcast_mask & 8) ? x : 0]);
+
     }
     out += blockDim.y * arg.shifts[0][0];
     x1 += blockDim.y * arg.shifts[1][0];
@@ -253,7 +240,7 @@ __global__ void FallbackLaunchFusedMulAdd2Kernel(T* out, const T* x1,
   }
 }
 
-template <typename T, int SGN>
+template <typename T, FMAType Type>
 __global__ void FallbackLaunchFusedMulAdd2Kernel2D(T* out, const T* x1,
                                                    const T* y1, const T* x2,
                                                    const T* y2,
@@ -265,16 +252,15 @@ __global__ void FallbackLaunchFusedMulAdd2Kernel2D(T* out, const T* x1,
   y2 += blockIdx.x * arg.shifts[4][0];
 
   for (int32 x = threadIdx.x; x < arg.dims[0]; x += blockDim.x) {
-    T m1 = x1[(arg.broadcast_mask & 1) ? x : 0] *
-           y1[(arg.broadcast_mask & 2) ? x : 0];
-    T m2 = x2[(arg.broadcast_mask & 4) ? x : 0] *
-           y2[(arg.broadcast_mask & 8) ? x : 0];
-    out[x] = (SGN > 0) ? m1 + m2 : m1 - m2;
+    out[x]=fma_op<Type>(x1[(arg.broadcast_mask & 1) ? x : 0] *
+           y1[(arg.broadcast_mask & 2) ? x : 0],
+           x2[(arg.broadcast_mask & 4) ? x : 0] *
+           y2[(arg.broadcast_mask & 8) ? x : 0]);
   }
 }
 
-template <typename T, int SGN>
-void FallbackLaunchFusedMulAddOp<GPUDevice, T, SGN>::operator()(
+template <typename T, FMAType Type>
+void FallbackLaunchFusedMulAddOp<GPUDevice, T, Type>::operator()(
     const GPUDevice& device, T* out, const T* x1, const T* y1, const T* x2,
     int64 dims[5], uint8 broadcast_masks[5]) {
   // printf("FallbackFusedMulAdd %d %d %d\n", broadcast_masks[0],
@@ -315,21 +301,21 @@ void FallbackLaunchFusedMulAddOp<GPUDevice, T, SGN>::operator()(
   // TODO: cover the case where dim[0]>>256 but dim[2,3,4] are small
   // (likely performance issue due to a small number of workgroups)
   if (dims[2] == 1 && dims[3] == 1 && dims[4] == 1) {
-    TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAddKernel2D<T, SGN>,
+    TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAddKernel2D<T, Type>,
                                 dim3(dims[1], 1, 1),
                                 dim3(dims[0] > 256 ? 256 : dims[0], 1, 1), 0,
                                 device.stream(), out, x1, y1, x2, arg));
-  } else if(dims[4]==1 && dims[0]/256 > dims[2]*dims[3]) {
+  } else if (dims[4] == 1 && dims[0] / 256 > dims[2] * dims[3]) {
     int block_x = min(256, dims[0]);
     int block_y = min(256 / block_x, dims[1]);
     int block_z = min(256 / (block_x * block_y), dims[2]);
     int grid_x = (dims[2] + block_z - 1) / block_z;
     int grid_y = dims[3];
-    int grid_z = max(1, dims[0]/256);
+    int grid_z = max(1, dims[0] / 256);
     // printf("%d %d %d x %d %d %d\n", block_x, block_y, block_z, grid_x,
     // grid_y, grid_z);
 
-    TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAddKernel4D<T, SGN>,
+    TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAddKernel4D<T, Type>,
                                 dim3(grid_x, grid_y, grid_z),
                                 dim3(block_x, block_y, block_z), 0,
                                 device.stream(), out, x1, y1, x2, arg));
@@ -343,15 +329,15 @@ void FallbackLaunchFusedMulAddOp<GPUDevice, T, SGN>::operator()(
     // printf("%d %d %d x %d %d %d\n", block_x, block_y, block_z, grid_x,
     // grid_y, grid_z);
 
-    TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAddKernel<T, SGN>,
+    TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAddKernel<T, Type>,
                                 dim3(grid_x, grid_y, grid_z),
                                 dim3(block_x, block_y, block_z), 0,
                                 device.stream(), out, x1, y1, x2, arg));
   }
 }
 
-template <typename T, int SGN>
-void FallbackLaunchFusedMulAdd2Op<GPUDevice, T, SGN>::operator()(
+template <typename T, FMAType Type>
+void FallbackLaunchFusedMulAdd2Op<GPUDevice, T, Type>::operator()(
     const GPUDevice& device, T* out, const T* x1, const T* y1, const T* x2,
     const T* y2, int64 dims[5], uint8 broadcast_masks[5]) {
   // printf("FallbackFusedMulAdd2 %d %d %d\n", broadcast_masks[0],
@@ -395,7 +381,7 @@ void FallbackLaunchFusedMulAdd2Op<GPUDevice, T, SGN>::operator()(
       (broadcast_masks[1] & 4) ? ((broadcast_masks[0] & 4) ? dims[0] : 1) : 0;
   arg.shifts[4][0] =
       (broadcast_masks[1] & 8) ? ((broadcast_masks[0] & 8) ? dims[0] : 1) : 0;
-  TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAdd2Kernel<T, SGN>,
+  TF_CHECK_OK(GpuLaunchKernel(FallbackLaunchFusedMulAdd2Kernel<T, Type>,
                               dim3(grid_x, grid_y, grid_z),
                               dim3(block_x, block_y, block_z), 0,
                               device.stream(), out, x1, y1, x2, y2, arg));
@@ -415,15 +401,15 @@ void FallbackLaunchFusedMulAdd2Op<GPUDevice, T, SGN>::operator()(
       const GPUDevice& device, X* out, const X* x1, const X* y1, const X* x2, \
       const X* y2, int64 dims[5], uint8 broadcast_masks[5]);
 
-INSTANTIATE_FMA(Eigen::half, 1);
-INSTANTIATE_FMA(float, 1);
-INSTANTIATE_FMA(double, 1);
-INSTANTIATE_FMA(Eigen::half, 0);
-INSTANTIATE_FMA(float, 0);
-INSTANTIATE_FMA(double, 0);
-INSTANTIATE_FMA(Eigen::half, -1);
-INSTANTIATE_FMA(float, -1);
-INSTANTIATE_FMA(double, -1);
+INSTANTIATE_FMA(Eigen::half, FMAType_Add);
+INSTANTIATE_FMA(float, FMAType_Add);
+INSTANTIATE_FMA(double, FMAType_Add);
+INSTANTIATE_FMA(Eigen::half, FMAType_SubRev);
+INSTANTIATE_FMA(float, FMAType_SubRev);
+INSTANTIATE_FMA(double, FMAType_SubRev);
+INSTANTIATE_FMA(Eigen::half, FMAType_Sub);
+INSTANTIATE_FMA(float, FMAType_Sub);
+INSTANTIATE_FMA(double, FMAType_Sub);
 
 };  // namespace tensorflow
 
