4808bd3973b453b11400d06db7717bfcb99b7f05
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2020-05-11 21:03:34 +0000

    Resetting the dropout seed every call

--- tensorflow/core/kernels/dropout_op.cc
+++ tensorflow/core/kernels/dropout_op.cc
@@ -20,6 +20,7 @@ limitations under the License.
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/kernels/random_op.h"
+#include "tensorflow/core/platform/random.h"
 #include "tensorflow/core/platform/stream_executor.h"
 #include "tensorflow/core/util/guarded_philox_random.h"
 #include "tensorflow/core/util/tensor_format.h"
@@ -111,7 +112,10 @@ class DropoutOp : public OpKernel {
     else
       seed = in3.scalar<int64>()();
     // don't reset the seed for every call unless it is explicitly non-0
-    if (seed != 0) generator_.ResetSeeds(seed, 0);
+    if (seed != 0)
+      generator_.ResetSeeds(seed, 0);
+    else
+      generator_.ResetSeeds(random::New64(), 0);
 
     typedef random::UniformDistribution<random::PhiloxRandom, float>
         Distribution;
