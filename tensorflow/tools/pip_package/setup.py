17f8d3cae582f6d4072537ee31ce6ce67c4839c0
sunway513 <peng.sun@amd.com>
2020-09-21 19:25:57 -0500

    Bump the version to 1.15.2, to build with ROCm3.8 user bits

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -48,7 +48,7 @@ DOCLINES = __doc__.split('\n')
 # Also update tensorflow/tensorflow.bzl and
 # tensorflow/core/public/version.h
 
-_VERSION = '1.15.1'
+_VERSION = '1.15.2'
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.7.0',
