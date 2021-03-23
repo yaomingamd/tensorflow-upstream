912b044fbd4bbf262938ab990945c788499b955c
sunway513 <peng.sun@amd.com>
2021-03-23 10:44:30 -0500

    bump up the version to build with ROCm4.1

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -48,7 +48,7 @@ DOCLINES = __doc__.split('\n')
 # Also update tensorflow/tensorflow.bzl and
 # tensorflow/core/public/version.h
 
-_VERSION = '1.15.5'
+_VERSION = '1.15.6'
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.7.0',
