d5e19ab49abcfbb6351fc7ca625cb9fe2c3921cc
Michael Melesse <micmelesse@gmail.com>
2021-08-26 13:20:44 -0500

    add lu_op_test

--- tensorflow/tools/ci_build/linux/rocm/run_gpu_single.sh
+++ tensorflow/tools/ci_build/linux/rocm/run_gpu_single.sh
@@ -44,7 +44,7 @@ yes "" | $PYTHON_BIN_PATH configure.py
 bazel test \
       --config=rocm \
       -k \
-      --test_tag_filters=-no_oss,-oss_serial,-no_gpu,-no_rocm,-benchmark-test,-v1only \
+      --test_tag_filters=gpu,-no_oss,-oss_serial,-no_gpu,-no_rocm,-benchmark-test,-v1only \
       --jobs=${N_BUILD_JOBS} \
       --local_test_jobs=${N_TEST_JOBS} \
       --test_env=TF_GPU_COUNT=$TF_GPU_COUNT \
@@ -61,4 +61,5 @@ bazel test \
       -//tensorflow/core/tpu/... \
       -//tensorflow/lite/... \
       -//tensorflow/compiler/tf2tensorrt/... \
+      -//tensorflow/python/kernel_tests:lu_op_test \
 
