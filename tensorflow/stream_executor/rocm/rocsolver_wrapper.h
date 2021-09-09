baca54f0fd1976a2e96919301df34fa3a9e2002f
Michael Melesse <micmelesse@gmail.com>
2021-09-09 15:06:46 -0500

    op_gpu compilable

--- tensorflow/stream_executor/rocm/rocsolver_wrapper.h
+++ tensorflow/stream_executor/rocm/rocsolver_wrapper.h
@@ -79,11 +79,23 @@ namespace wrap {
   __macro(rocsolver_dgetrs_batched)         \
   __macro(rocsolver_sgetrs_batched)         \
   __macro(rocsolver_zgetrs_batched)         \
-  __macro(rocsolver_cpotrf)         	    \
+  __macro(rocsolver_cpotrf)         	      \
   __macro(rocsolver_dpotrf)                 \
   __macro(rocsolver_spotrf)                 \
-  __macro(rocsolver_zpotrf)
+  __macro(rocsolver_zpotrf)                 \
+  __macro(rocsolver_cpotrf_batched)         	      \
+  __macro(rocsolver_dpotrf_batched)                 \
+  __macro(rocsolver_spotrf_batched)                 \
+  __macro(rocsolver_zpotrf_batched)                 
 
+  // __macro(rocsolver_cpotrs)         	      \
+  // __macro(rocsolver_dpotrs)                 \
+  // __macro(rocsolver_spotrs)                 \
+  // __macro(rocsolver_zpotrs)                 \
+  // __macro(rocsolver_cpotrs_batched)         	      \
+  // __macro(rocsolver_dpotrs_batched)                 \
+  // __macro(rocsolver_spotrs_batched)                 \
+  // __macro(rocsolver_zpotrs_batched)                 
 // clang-format on
 
 FOREACH_ROCSOLVER_API(ROCSOLVER_API_WRAPPER)
