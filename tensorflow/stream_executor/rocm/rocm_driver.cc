5e8f96e5c730d262b99475fecbd64aa13a14cbf1
jayfurmanek <Jason.Furmanek@amd.com>
2021-11-05 19:54:52 +0000

    Enable managed malloc APIs for ROCm

--- tensorflow/stream_executor/rocm/rocm_driver.cc
+++ tensorflow/stream_executor/rocm/rocm_driver.cc
@@ -642,15 +642,32 @@ GpuDriver::ContextGetSharedMemConfig(GpuContext* context) {
                                                     uint64_t bytes) {
   ScopedActivateContext activated{context};
 
-  LOG(ERROR)
-      << "Feature not supported on ROCm platform (UnifiedMemoryAllocate)";
-  return nullptr;
+  hipDeviceptr_t result = 0;
+  // "Portable" memory is visible to all ROCm contexts. Safe for our use model.
+  hipError_t res = hipMallocManaged(&result, bytes, hipMemAttachGlobal);
+  if (res != hipSuccess) {
+    LOG(ERROR) << "failed to alloc " << bytes
+               << " bytes unified memory; result: " << ToString(res);
+    return nullptr;
+  }
+  void* ptr = reinterpret_cast<void*>(result);
+  VLOG(2) << "allocated " << ptr << " for context " << context->device_ordinal()
+          << " of " << bytes << " bytes in unified memory";
+  return ptr;
 }
 
 /* static */ void GpuDriver::UnifiedMemoryDeallocate(GpuContext* context,
                                                      void* location) {
-  LOG(ERROR)
-      << "Feature not supported on ROCm platform (UnifiedMemoryDeallocate)";
+  ScopedActivateContext activation(context);
+  hipDeviceptr_t pointer = absl::bit_cast<hipDeviceptr_t>(location);
+  hipError_t res = hipFree(pointer);
+  if (res != hipSuccess) {
+    LOG(ERROR) << "failed to free unified memory at " << location
+               << "; result: " << ToString(res);
+  } else {
+    VLOG(2) << "deallocated unified memory at " << location << " for context "
+            << context->device_ordinal();
+  }
 }
 
 /* static */ void* GpuDriver::HostAllocate(GpuContext* context,
