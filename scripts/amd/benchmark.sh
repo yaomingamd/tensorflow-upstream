0d3d3ed4decea40f56c0776b83cc427f617ba25b
Michael Melesse <micmelesse@gmail.com>
2022-02-08 12:16:18 -0600

    limit to one step

--- scripts/amd/benchmark.sh
+++ scripts/amd/benchmark.sh
@@ -5,7 +5,6 @@ rm -rf $LOG_DIR
 mkdir -p $LOG_DIR
 chmod -R 777 $LOG_DIR
 
-
 # export AMD_OCL_WAIT_COMMAND=1
 # export AMD_LOG_LEVEL=3
 # export HIP_LAUNCH_BLOCKING=1
@@ -16,22 +15,20 @@ options=""
 
 options="$options --model=resnet50_v1.5"
 
-
 # options="$options --xla"
 options="$options --xla_compile"
 # options="$options --compute_lr_on_cpu"
 
 options="$options --use_fp16"
 
-
 options="$options --batch_size=256"
-
-
 
 options="$options --print_training_accuracy"
 
+options="$options --num_batches=1"
 
 export $env_vars
 
 cd /dockerx/benchmarks
-python3 scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py $options | tee $LOG_DIR/tf_cnn_benchmarks.log
+# python3 scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py --help
+python3 scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py $options 2>&1 | tee $LOG_DIR/tf_cnn_benchmarks.log
