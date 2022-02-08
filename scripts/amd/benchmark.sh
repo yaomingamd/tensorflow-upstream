2abfbf698d31000642d35c2ec63823bcdbbba0e7
Michael Melesse <micmelesse@gmail.com>
2022-02-08 12:03:24 -0600

    add benchmark script

---
+++ scripts/amd/benchmark.sh
@@ -0,0 +1,37 @@
+ROOT_DIR=$(pwd)
+DEFAULT_LOG_DIR=$ROOT_DIR/$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+LOG_DIR="${1:-$DEFAULT_LOG_DIR}"
+rm -rf $LOG_DIR
+mkdir -p $LOG_DIR
+chmod -R 777 $LOG_DIR
+
+
+# export AMD_OCL_WAIT_COMMAND=1
+# export AMD_LOG_LEVEL=3
+# export HIP_LAUNCH_BLOCKING=1
+
+env_vars=""
+
+options=""
+
+options="$options --model=resnet50_v1.5"
+
+
+# options="$options --xla"
+options="$options --xla_compile"
+# options="$options --compute_lr_on_cpu"
+
+options="$options --use_fp16"
+
+
+options="$options --batch_size=256"
+
+
+
+options="$options --print_training_accuracy"
+
+
+export $env_vars
+
+cd /dockerx/benchmarks
+python3 scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py $options | tee $LOG_DIR/tf_cnn_benchmarks.log
