d4388c980233dc2e8400d604cdb0532b180fc34f
Michael Melesse <micmelesse@gmail.com>
2022-03-28 17:19:55 -0500

    compress to nccl function

--- scripts/amd/rccl_xla_test.sh
+++ scripts/amd/rccl_xla_test.sh
@@ -29,8 +29,8 @@ chmod -R 777 $LOG_DIR
 export XLA_FLAGS="--xla_dump_hlo_as_text --xla_dump_to=$LOG_DIR/xla" 
 python3 scripts/amd/rccl_xla_script_new.py --log_dir $LOG_DIR 2>&1 | tee $LOG_DIR/rccl_script.log
 
-echo "KERNELS:"
-cat $LOG_DIR/rccl_script.log | grep ShaderName
+echo "NCCL KERNELS:"
+cat $LOG_DIR/rccl_script.log | grep nccl
 
 chmod -R 777 $LOG_DIR
 
