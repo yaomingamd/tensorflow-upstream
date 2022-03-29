58d0c9c63d73c002de177aaebb47354bfd32512f
Michael Melesse <micmelesse@gmail.com>
2022-03-29 13:57:14 -0500

    have on tf function

--- scripts/amd/rccl_xla_script_new.py
+++ scripts/amd/rccl_xla_script_new.py
@@ -12,60 +12,59 @@ tf.config.optimizer.set_jit(True)
 def main(log_dir):
 
     # set strategy
+    gpus = ["GPU:0", "GPU:1", "GPU:2", "GPU:3"][:2]
     strategy = tf.distribute.MirroredStrategy(
-        cross_device_ops=tf.distribute.NcclAllReduce(), devices=["GPU:0", "GPU:1"])
+        cross_device_ops=tf.distribute.NcclAllReduce(), devices=gpus)
     print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
     # model, optimizer, and checkpoint must be created under `strategy.scope`.
     with strategy.scope():
-
         # create model
         model = tf.keras.Sequential([
             tf.keras.layers.Dense(2)
         ])
         optimizer = tf.keras.optimizers.SGD()
 
-        def train_step(inputs):
-            images, labels = inputs
-            loss_object = tf.keras.losses.MeanSquaredError(
-                reduction=tf.keras.losses.Reduction.NONE)
-
-            with tf.GradientTape() as tape:
-                predictions = model(images, training=True)
-                loss = tf.nn.compute_average_loss(loss_object(
-                    labels, predictions), global_batch_size=strategy.num_replicas_in_sync)
-
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(
-                zip(gradients, model.trainable_variables))
-            return loss
-
-        # `run` replicates the provided computation and runs it
-        # with the distributed input.
+        # train function
         @tf.function
         def distributed_train_step(dataset_inputs):
+            def train_step(inputs):
+                images, labels = inputs
+                loss_object = tf.keras.losses.MeanSquaredError(
+                    reduction=tf.keras.losses.Reduction.NONE)
+
+                with tf.GradientTape() as tape:
+                    predictions = model(images, training=True)
+                    loss = tf.nn.compute_average_loss(loss_object(
+                        labels, predictions), global_batch_size=strategy.num_replicas_in_sync)
+
+                gradients = tape.gradient(loss, model.trainable_variables)
+                optimizer.apply_gradients(
+                    zip(gradients, model.trainable_variables))
+                return loss
+
             per_replica_losses = strategy.run(
                 train_step, args=(dataset_inputs,))
-            # print("per_replica_losses", per_replica_losses)
-            return strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_losses,
+            return strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_losses,
                                    axis=None)
 
+        # run train function
         train_log_dir = os.path.join(log_dir, "ckpts")
         train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 
-        # Bracket the function call with
-        tf.summary.trace_on(graph=True, profiler=True)
         dataset_inputs = tf.constant(
-            [[1.0, 2.0, 3.0, 4.0, 5.0]]), tf.constant([2.0])
+            [[1.0]]), tf.constant([2.0])
+        tf.summary.trace_on(graph=True, profiler=True)
         loss = distributed_train_step(dataset_inputs)
         step = 1
-        print("Step {}, Loss: {}".format(step, loss))
 
         with train_summary_writer.as_default():
             tf.summary.trace_export(
                 name="my_func_trace",
                 step=step,
                 profiler_outdir=train_log_dir)
+
+        print("Step {}, Loss: {}".format(step, loss))
 
 
 if __name__ == '__main__':
