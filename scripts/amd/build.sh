629fb2b64a212b37cd901258b4377a22f46bdd47
Michael Melesse <micmelesse@gmail.com>
2022-04-22 14:39:36 -0500

    uninstall tf nightly in the autobuilds

--- scripts/amd/build.sh
+++ scripts/amd/build.sh
@@ -3,12 +3,12 @@
 # sudo apt-get update && sudo apt-get install -y openjdk-8-jdk openjdk-8-jre unzip wget git
 # cd ~ && rm -rf bazel*.sh && wget https://github.com/bazelbuild/bazel/releases/download/3.1.0/bazel-3.1.0-installer-linux-x86_64.sh  && bash bazel*.sh && rm -rf ~/*.sh
 
-
 ROOT_DIR=$(pwd)
 
 (cd "/usr/local/lib/bazel/bin" && curl -fLO https://releases.bazel.build/5.1.1/release/bazel-5.1.1-linux-x86_64 && chmod +x bazel-5.1.1-linux-x86_64)
 
 cd $ROOT_DIR
 bazel clean
-pip3 uninstall tensorflow -y
+pip3 uninstall tensorflow tf-nightly-rocm -y
 scripts/amd/build_rocm_python3.sh
+pip3 list | grep '^t.*'
