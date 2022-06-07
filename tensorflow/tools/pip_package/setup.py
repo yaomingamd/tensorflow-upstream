4688b5e941a341af56848b794ec6d2efecb9055b
Rohit Santhanam <rohit.santhanam@amd.com>
2022-06-07 15:14:56 +0000

    Fix pip package setup protobuf version issue.

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -87,7 +87,7 @@ REQUIRED_PACKAGES = [
     'numpy >= 1.20',
     'opt_einsum >= 2.3.2',
     'packaging',
-    'protobuf >= 3.9.2',
+    'protobuf >= 3.9.2, < 3.20',
     'setuptools',
     'six >= 1.12.0',
     'termcolor >= 1.1.0',
