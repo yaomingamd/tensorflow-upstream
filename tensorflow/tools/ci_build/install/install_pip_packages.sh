a9b7eb4a6cd7ce47f45d3f1eaf672f9ac9bee120
Jason Furmanek <Jason.Furmanek@amd.com>
2022-06-07 16:08:28 +0000

    freeze keras-nightly version for QA-53 branch

--- tensorflow/tools/ci_build/install/install_pip_packages.sh
+++ tensorflow/tools/ci_build/install/install_pip_packages.sh
@@ -95,7 +95,7 @@ pip3 install --upgrade gast
 pip3 install --upgrade termcolor
 
 # Keras
-pip3 install keras-nightly --no-deps
+pip3 install keras-nightly==2.10.0.dev2022060207 --no-deps
 pip3 install keras_preprocessing==1.1.0 --no-deps
 pip3 install --upgrade h5py==3.1.0
 
