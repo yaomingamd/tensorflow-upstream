2ac52f9b2a41665184c9b85bae01f1b3ef24466d
Augie Fackler <augie@google.com>
2022-11-02 14:27:17 -0700

    Integrate LLVM at llvm/llvm-project@e57ab8fe91f0
    Updates LLVM usage to match
    [e57ab8fe91f0](https://github.com/llvm/llvm-project/commit/e57ab8fe91f0)
    
    PiperOrigin-RevId: 485695599

--- tensorflow/compiler/mlir/tosa/transforms/legalize_tfl.cc
+++ tensorflow/compiler/mlir/tosa/transforms/legalize_tfl.cc
@@ -3123,7 +3123,8 @@ LogicalResult ConvertTFLCustomOp::matchAndRewrite(
     Operation* op, PatternRewriter& rewriter) const {
   auto tfl_custom_op = cast<TFL::CustomOp>(op);
   rewriter.replaceOpWithNewOp<tosa::CustomOp>(
-      op, op->getResultTypes(), tfl_custom_op.custom_code(), op->getOperands());
+      op, op->getResultTypes(), tfl_custom_op.custom_code(), StringRef{},
+      StringRef{}, op->getOperands());
 
   return success();
 }
