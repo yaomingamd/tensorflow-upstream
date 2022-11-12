55196e345240af14ca6332c71682c6bc7fd7a786
Michael Melesse <micmelesse@gmail.com>
2022-11-12 17:10:59 -0600

    clean up

--- scripts/amd/docker_build.sh
+++
@@ -1,4 +0,0 @@
-cd tensorflow/tools/tf_sig_build_dockerfiles
-docker build -f Dockerfile.rocm --build-arg ROCM_VERSION=5.3.0 --build-arg PYTHON_VERSION=3.10 -t tf_centos . \
-	2>&1 | tee $LOG_DIR/build.log
-# docker build -f Dockerfile.centos -t tf_centos .  2>&1 | tee $LOG_DIR/centos_build.log
