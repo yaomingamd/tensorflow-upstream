4b6a24969ebd48864c10aa989f26ebd35b5024e8
Michael Melesse <micmelesse@gmail.com>
2022-11-15 13:48:53 -0600

    update README and create rocm verify script

--- scripts/amd/docker_exec.sh
+++ scripts/amd/docker_exec.sh
@@ -2,10 +2,6 @@ set -ex
 
 # configure tensorflow for ROCM
 docker exec tf \
-	sed -i "s/build:rbe_linux_rocm_base --action_env=TF_ROCM_CONFIG_REPO=\"@ubuntu20.04-gcc9_manylinux2014-rocm_config_rocm\"/build:rbe_linux_rocm_base --action_env=TF_ROCM_CONFIG_REPO=\"@ubuntu20.04-gcc9_manylinux2014-rocm_config_rocm\"\nbuild:rbe_linux_rocm_base --action_env=TF_ROCM_GCC=1/" \
-	/tf/tensorflow/.bazelrc
-
-docker exec tf \
 	/tf/tensorflow/configure
 
 # build tensorflow
@@ -22,7 +18,5 @@ docker exec tf \
 	--project_name tensorflow_rocm
 
 # check wheel
-docker exec tf bash -c \
-	'TF_WHEEL=$(ls -Art /tf/pkg/tensorflow*.whl) && NEW_TF_WHEEL=${TF_WHEEL/linux/"manylinux2014"} && 
-	mv $TF_WHEEL $NEW_TF_WHEEL && 
-	auditwheel show $NEW_TF_WHEEL'
+docker exec tf \
+	/usertools/rename_and_verify_ROCM_wheels.sh
