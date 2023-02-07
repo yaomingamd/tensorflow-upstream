acde99e21179d4e6626c006be14100f97666f917
Wang, Yanyao <yanyao.wang@amd.com>
2023-02-06 16:17:46 -0800

    fix script

--- tensorflow/tools/tf_sig_build_dockerfiles/compute-rocm-dkms-no-npi-hipclang@11574.centos7.bash
+++ tensorflow/tools/tf_sig_build_dockerfiles/compute-rocm-dkms-no-npi-hipclang@11574.centos7.bash
@@ -1,6 +1,7 @@
 set -x
 
 rocm_build=${0%%.*}
+rocm_build=${rocm_build//\//}
 
 yum install -y http://artifactory-cdn.amd.com/artifactory/list/amdgpu-rpm/rhel/amd-nonfree-radeon-7-1.noarch.rpm
 amdgpu-repo --rocm-build=${rocm_build//@/\/}
