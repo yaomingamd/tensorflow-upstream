c5923c6cd2d084b4d4aabb07c86a30305a8662e7
Rahul Batra <rahbatra@amd.com>
2023-04-27 05:03:07 +0000

    Hardcode MIOpen to run in immediate mode

--- tensorflow/compiler/xla/stream_executor/rocm/rocm_dnn.cc
+++ tensorflow/compiler/xla/stream_executor/rocm/rocm_dnn.cc
@@ -716,12 +716,13 @@ MIOpenSupport::MIOpenSupport(GpuExecutor* parent) : parent_(parent) {
                                       &return_best_algo_only_));
 
   // by default, use Find Mode APIs for convolution
-  use_immediate_mode_ = false;
+  //use_immediate_mode_ = false;
   // swich to Find Mode if env var TF_ROCM_USE_IMMEDIATE_MODE is set
 
   TF_CHECK_OK(tsl::ReadBoolFromEnvVar("TF_ROCM_USE_IMMEDIATE_MODE", false,
                                       &use_immediate_mode_));
 
+  use_immediate_mode_ = true;
   bool enable_pooling_cache = false;
   TF_CHECK_OK(tsl::ReadBoolFromEnvVar("TF_ROCM_BW_POOL_CACHE", false,
                                       &enable_pooling_cache));
