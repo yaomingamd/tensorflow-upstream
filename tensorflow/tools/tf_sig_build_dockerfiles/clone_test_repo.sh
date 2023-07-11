55dc5e1dfd17358635e46ac6522fb8bbeabcecc7
Yanyao Wang <wangyanyao@msn.com>
2023-07-10 22:37:04 -0500

    Update benchmarsks branch when TF version is higher than 2.12 (#2154)
    Signed-off-by: Wang, Yanyao <yanyao.wang@amd.com>
    Co-authored-by: Wang, Yanyao <yanyao.wang@amd.com>

--- tensorflow/tools/tf_sig_build_dockerfiles/clone_test_repo.sh
+++ tensorflow/tools/tf_sig_build_dockerfiles/clone_test_repo.sh
@@ -7,6 +7,7 @@ cd "${_DL_FLD}" || exit 2
 clone_benchmark() {
     local benchmarks_repo='https://github.com/tensorflow/benchmarks'
     local tf_ver
+    local benchmark_branch
     tf_ver=$(grep -oP "_VERSION = '\K[0-9]+\.[0-9]+" \
         "${_DL_FLD}/tensorflow/tensorflow/tools/pip_package/setup.py")
     if [ -z "${tf_ver}" ]; then
@@ -18,8 +19,9 @@ clone_benchmark() {
     # For TF version higher than 2.12, use the new AMD benchmarks repo
     if (( ${tf_ver//./} > 212 ));then
         benchmarks_repo='https://github.com/ROCmSoftwarePlatform/benchmarks'
+        benchmark_branch='-b tf2.13-compatible'
     fi
-    git clone ${benchmarks_repo}
+    git clone ${benchmark_branch} ${benchmarks_repo}
 }
 
 git clone https://github.com/tensorflow/models.git
